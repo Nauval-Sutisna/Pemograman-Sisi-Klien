35a3c5074efa1d1ddc4703ca826e654a31a443dc
Nauval-Sutisna <nauval.master99@gmail.com>
2025-10-02 11:46:12 +0700

    Update App.jsx

--- Pertemuan-3/src/App.jsx
+++ Pertemuan-3/src/App.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './package/Home.jsx'
 import './App.css'
+import Login from './package/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <Login />
   </StrictMode>,
 )
