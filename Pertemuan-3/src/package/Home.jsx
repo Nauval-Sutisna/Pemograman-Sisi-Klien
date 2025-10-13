c0a91d63c0569ef72e953e6481f6108819b5281e
Nauval-Sutisna <nauval.master99@gmail.com>
2025-10-13 17:09:02 +0700

    Update Home.jsx

--- Pertemuan-3/src/package/Home.jsx
+++ Pertemuan-3/src/package/Home.jsx
@@ -6,7 +6,7 @@
 function Home () {
     return  <h1 class="text-3x1 font-bold underline">
                 Hello World!
-            </h1>
+            </h1>;
             // <div>
             //     <Judul />
             //     <Isi />
