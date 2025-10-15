f756b83198a4a623975b3cf739e28efbf7c80ad1
Nauval-Sutisna <nauval.master99@gmail.com>
2025-10-15 23:29:01 +0700

    Update AuthLayout.jsx

--- Pertemuan-3/src/Pages/Layouts/AuthLayout.jsx
+++ Pertemuan-3/src/Pages/Layouts/AuthLayout.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+// import React from "react";
 
-const AuthLayout = ({ children }) => {
+// const AuthLayout = ({ children }) => {
+//   return (
+//     <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+//       <div className="w-full max-w-md p-6">{children}</div>
+//     </div>
+//   );
+// };
+
+// export default AuthLayout;
+
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const AuthLayout = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex justify-center items-center">
-      <div className="w-full max-w-md p-6">{children}</div>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <Outlet />
     </div>
   );
 };
